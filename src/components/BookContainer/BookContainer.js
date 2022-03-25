046738e9b25fcb21137a3f96cb1ad39fe7e6b484
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-03-26 01:18:04 +0600

    done show cart remove all card

--- src/components/BookContainer/BookContainer.js
+++ src/components/BookContainer/BookContainer.js
@@ -1,26 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import ShowBook from '../ShowBooks/ShowBook';
+import ShowCart from '../ShowCart/ShowCart';
 import './BookContainer.css';
 
 const BookContainer = () => {
   const [books, setBooks] = useState([]);
+  const [carts, setCart] = useState([]);
   useEffect(() => {
     fetch('data.json')
       .then((res) => res.json())
       .then((data) => setBooks(data));
   }, []);
+
+  const addToCart = (book) => {
+    
+    const newCart = [...carts, book]; 
+    setCart(newCart);
+  }
+  const chooseAgain = () => {
+    const newCart = [];
+    setCart(newCart)
+  }
   return (
     <div className="container">
       <div className="book-container">
               <div className='show-book'>
                   {
-                     books.map(book => <ShowBook book={book} key = {book.id}></ShowBook>)
+                     books.map(book => <ShowBook book={book} key = {book.id} addToCart= {addToCart}></ShowBook>)
                   }
-              </div>
+        </div>
+        <div className='show-selected'>
+          <h2>Selected books</h2>
+          {
+            carts.map(cart => <ShowCart cart ={cart} key ={cart.id} ></ShowCart> )
+          }
+          <button className='choose-again' onClick={() => chooseAgain()}>Choose Again</button>
+        </div>
+        
         <div>
-          <h1>This is right side of book choose and clear</h1>
+          
         </div>
       </div>
+
     </div>
   );
 };
--- src/components/ShowBooks/ShowBook.js
+++ src/components/ShowBooks/ShowBook.js
@@ -1,24 +1,39 @@
+
 import React from 'react';
 import './ShowBook.css';
 
 const ShowBook = (props) => {
-  console.log(props);
-  const { id, name, price, img } = props.book;
-  console.log(id, name, price, img);
+ 
+  const { name, price, img } = props.book;
+  const { addToCart } = props;
+  const book = props.book
+  
+  
+  // const { addToCart } = props.addToCart; 
 
   return (
     <div className="card-container">
-      <div className='img-container'>
+      <div className="img-container">
         <img className="img-fluid" src={img} alt={name} />
       </div>
 
-      <div className='card-info'>
+      <div className="card-info">
         <h3> {name} </h3>
-        <h4>Price: ${ price}</h4>
+        <h4>Price: ${price}</h4>
       </div>
 
-      <button className='add-cart-btn'>
+      <button className="add-cart-btn" onClick={() => addToCart(book)} >
         <p>Add to Cart</p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="25"
+          height="25"
+          fill="currentColor"
+          className="bi bi-cart-fill"
+          viewBox="0 0 16 16"
+        >
+          <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+        </svg>
       </button>
     </div>
   );
